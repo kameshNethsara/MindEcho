09d85bc4050bb83937ffd37864d0219bbdcd0694
kameshNethsara <kamesh317nethsara@gmail.com>
2025-11-15 18:43:16 +0530

    feat: implement JWT token signing functions for access and refresh tokens

---
+++ backend/src/utils/tokens.ts
@@ -0,0 +1,29 @@
+import { IUSER } from "../models/user.model";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
+
+export const signAccessToken = (user: IUSER): string => { 
+    return jwt.sign(
+        {
+            sub: user._id.toString(),
+            roles: user.roles
+        },JWT_SECRET,{
+            expiresIn: "3h"
+        }
+    )
+}
+
+export const signRefreshToken = (user: IUSER): string => {
+    return jwt.sign(
+        {
+            sub: user._id.toString(),
+            roles: user.roles
+        },JWT_REFRESH_SECRET,{
+            expiresIn: "7d"
+        }
+    )
+}
