ed2263046efd7b15f022defec2be8c9066cc9492
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-02 14:07:33 +0530

    feat(tokens): update token expiration times and add console logs for debugging

--- backend/src/utils/tokens.ts
+++ backend/src/utils/tokens.ts
@@ -7,23 +7,25 @@ const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
 
 export const signAccessToken = (user: IUSER): string => { 
+    console.log("AccessToken test 1-------------------")
     return jwt.sign(
         {
             sub: user._id.toString(),
             roles: user.roles
         },JWT_SECRET,{
-            expiresIn: "3h"
+            expiresIn: "3d" //3d
         }
     )
 }
 
 export const signRefreshToken = (user: IUSER): string => {
+    console.log("Refreshtoken test 1-------------------")
     return jwt.sign(
         {
             sub: user._id.toString(),
             roles: user.roles
         },JWT_REFRESH_SECRET,{
-            expiresIn: "7d"
+            expiresIn: "7d" 
         }
     )
 }
